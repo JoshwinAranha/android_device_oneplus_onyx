42c7527d4dd0516ab7b61cadc5ec430c5ebaa6b1
Joshwin <aranha.joshwin@gmail.com>
2017-02-07 22:01:39 +0530

    reDefined tree to work with DnD
    Change-Id: I9337eb4767f3b24c6ad298d16a184d7009c4ba97

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_onyx-user
-add_lunch_combo lineage_onyx-userdebug
-add_lunch_combo lineage_onyx-eng
+add_lunch_combo dnd_onyx-user
+add_lunch_combo dnd_onyx-userdebug
+add_lunch_combo dnd_onyx-eng
